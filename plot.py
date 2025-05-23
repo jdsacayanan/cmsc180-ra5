f145410a37eae26e8164adab90559170dd1e95d8
jdsacayanan <jadon.sacayanan@gmail.com>
2025-05-23 11:55:00 +0800

    report

--- plot.py
+++ plot.py
@@ -4,18 +4,18 @@ import numpy as np
 
 # Data (n, t, average runtime)
 data = [
-    [20000, 2, 0.577255],
-    [20000, 4, 0.296651],
-    [20000, 8, 0.293474],
-    [20000, 16, 0.290521],
-    [25000, 2, 0.920419],
-    [25000, 4, 0.489994],
-    [25000, 8, 0.470956],
-    [25000, 16, 0.450763],
-    [30000, 2, 1.324529],
-    [30000, 4, 0.659446],
-    [30000, 8, 0.656013],
-    [30000, 16, 0.651398],
+    [20000, 2, 78.539681],
+    [20000, 4, 64.164683],
+    [20000, 8, 55.379865],
+    [20000, 16, 55.277927],
+    [25000, 2, 122.330378],
+    [25000, 4, 100.341758],
+    [25000, 8, 86.407713],
+    [25000, 16, 85.870184],
+    [30000, 2, 178.102681],
+    [30000, 4, 144.264106],
+    [30000, 8, 124.376693],
+    [30000, 16, 124.021209],
 ]
 
 # Convert to NumPy arrays
@@ -36,7 +36,7 @@ ax.scatter(t_vals, n_vals, z_vals, color='red', s=40, label='Data Points')
 ax.set_xlabel('t (Threads)')
 ax.set_ylabel('n (Data Size)')
 ax.set_zlabel('Average Runtime (seconds)')
-ax.set_title('Runtime Trends over Data Size and Threads for the Slave')
+# ax.set_title('Runtime Trends over Data Size and Threads for the Slave')
 
 # Optional: Better viewing angle
 ax.view_init(elev=20, azim=-45)
